6d852fa2b8061310f79b8c6ef273479c65186f0f
kimulaco <kmrkdev@gmail.com>
2019-03-27 11:41:19 +0900

    update generate method

--- src/css-triangle.ts
+++ src/css-triangle.ts
@@ -1,17 +1,69 @@
 /**
+ * Option
+ * @interface
+ */
+interface Option {
+    width?: number|string;
+    height?: number|string;
+    color?: string;
+    direction?: string;
+}
+
+/**
  * CssTriangle
  * @constructor
  */
 class CssTriangle {
-    /**
-     * generate
-     * @param {object} option
-     * @param {string} option.color
-     * @param {number|string} option.width
-     * @param {number|string} option.height
-     */
-    generate(option) {
-        return {};
+    private option: Option = {
+        'width': 100,
+        'height': 100,
+        'color': '#999',
+        'direction': 'top'
+    }
+
+    public generate(option: Option): any {
+        this.option = Object.assign(this.option, option);
+
+        return {
+            'width': 0,
+            'height': 0,
+            'border-style': 'solid',
+            'border-color': 'transparent',
+            'border-width': this.getBorderWidth(),
+            ...this.getBorderDirection()
+        };
+    }
+
+    private getBorderWidth(): string {
+        const widthHerf = Number(this.option.width) / 2;
+        const heightHerf = Number(this.option.height) / 2;
+
+        switch (this.option.direction) {
+            case 'top':
+                return `0 ${widthHerf}px ${this.option.height}px ${widthHerf}px`;
+            case 'bottom':
+                return `${this.option.height}px ${widthHerf}px 0 ${widthHerf}px`;
+            case 'right':
+                return `${heightHerf}px 0 ${heightHerf}px ${this.option.width}px`;
+            case 'left':
+                return `${heightHerf}px ${this.option.width}px ${heightHerf}px 0`;
+            default:
+                return `0 ${widthHerf}px ${this.option.height}px ${widthHerf}px`;
+        }
+    }
+
+    private getBorderDirection(): any {
+        switch (this.option.direction) {
+            case 'top':
+                return { 'border-bottom-color': this.option.color };
+            case 'bottom':
+                return { 'border-top-color': this.option.color };
+            case 'right':
+                return { 'border-left-color': this.option.color };
+            case 'left':
+                return { 'border-right-color': this.option.color };
+            default:
+                return { 'border-bottom-color': this.option.color };        }
     }
 }
 
--- test/css-triangle.test.js
+++ test/css-triangle.test.js
@@ -1,13 +1,65 @@
 const cssTriangle = require('../lib/css-triangle');
 
-describe('cssTriangle', ()=>{
-    test('generate({\nwidth: string\n})', ()=>{
-        const cssProperties = cssTriangle.generate({
-            color: '#000',
-            width: 20,
-            height: 20
+describe('.generate()', ()=>{
+    describe('direction?: string', ()=>{
+        test('\'top\'', ()=>{
+            const cssProperties = cssTriangle.generate({
+                direction: 'top'
+            });
+
+            expect(cssProperties).toEqual({
+                'width': 0,
+                'height': 0,
+                'border-style': 'solid',
+                'border-width': '0 50px 100px 50px',
+                'border-color': 'transparent',
+                'border-bottom-color': '#999'
+            });
         });
 
-        expect(cssProperties).toBeTruthy();
+        test('\'bottom\'', ()=>{
+            const cssProperties = cssTriangle.generate({
+                direction: 'bottom'
+            });
+
+            expect(cssProperties).toEqual({
+                'width': 0,
+                'height': 0,
+                'border-style': 'solid',
+                'border-color': 'transparent',
+                'border-width': '100px 50px 0 50px',
+                'border-top-color': '#999'
+            });
+        });
+
+        test('\'right\'', ()=>{
+            const cssProperties = cssTriangle.generate({
+                direction: 'right'
+            });
+
+            expect(cssProperties).toEqual({
+                'width': 0,
+                'height': 0,
+                'border-style': 'solid',
+                'border-color': 'transparent',
+                'border-width': '50px 0 50px 100px',
+                'border-left-color': '#999'
+            });
+        });
+
+        test('\'left\'', ()=>{
+            const cssProperties = cssTriangle.generate({
+                direction: 'left'
+            });
+
+            expect(cssProperties).toEqual({
+                'width': 0,
+                'height': 0,
+                'border-style': 'solid',
+                'border-color': 'transparent',
+                'border-width': '50px 100px 50px 0',
+                'border-right-color': '#999'
+            });
+        });
     });
 });
